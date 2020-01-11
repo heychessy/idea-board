400f7960982869bf6d6281aa65a4cdb8278b099c
Chessy Baweja <baweja.chessy@gmail.com>
2020-01-11 15:47:19 +0400

    Updates

---
+++ precache-manifest.6af47e8cd2ab94fbd416d602c0b07269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac71a20746449ad6257562f31257d1e",
+    "url": "/idea-board/index.html"
+  },
+  {
+    "revision": "17151d866698b5cead18",
+    "url": "/idea-board/static/css/main.f2462f30.chunk.css"
+  },
+  {
+    "revision": "c2d0e76552e1d93443ed",
+    "url": "/idea-board/static/js/2.efa66d2d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/idea-board/static/js/2.efa66d2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17151d866698b5cead18",
+    "url": "/idea-board/static/js/main.56e6d6e0.chunk.js"
+  },
+  {
+    "revision": "8cefdd62902527a9e943",
+    "url": "/idea-board/static/js/runtime-main.2eb44d79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/idea-board/static/media/logo.5d5d9eef.svg"
+  }
+]);
